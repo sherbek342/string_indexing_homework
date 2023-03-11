5e63b07b690c6c989e8a8ad9f1a33c332b33cd87
sherbek342 <sherbekrustamov342@gmail.com>
2023-03-11 12:19:49 +0300

    888

--- Index08.py
+++ Index08.py
@@ -6,5 +6,17 @@ def main(s):
     Returns:
         int: answer
     """
+    if s[0] == "*":
+        return 1
+    if s[1] == "*":
+        return 2
+    if s[2] == "*":
+        return 3
+    if s[3] == "*":
+        return 4
+    if s[4] == "*":
+        return 5
+
     return
+print(main("pab*l"))
         
