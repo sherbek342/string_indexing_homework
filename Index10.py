dcb026f0e6cb02528df9f69467268b069f7c8d11
sherbek342 <sherbekrustamov342@gmail.com>
2023-03-11 12:21:32 +0300

    100

--- Index10.py
+++ Index10.py
@@ -6,4 +6,11 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    y1 = int(s[0])
+    y2 = int(s[1])
+    y3 = int(s[2])
+    y4 = int(s[3])
+    y5 = int(s[4])
+    a = y1+y2+y3+y4+y5
+    return a
+print(main("22345"))
